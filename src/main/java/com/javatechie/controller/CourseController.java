f33bcf25acd2e5934152366fb558ab835da6c2aa
Rahulsharma1199 <rahul.sharma.ag99@gmail.com>
2025-10-25 16:49:51 +0530

    update

--- src/main/java/com/javatechie/controller/CourseController.java
+++ src/main/java/com/javatechie/controller/CourseController.java
@@ -65,10 +65,8 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-       return "Hello Techie , AWS CICD Example working fine !";
+        return "Hello Techie , AWS CICD Example working fine !";
     }
-
-
-
+    
 
 }
